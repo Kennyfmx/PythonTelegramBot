0578db486f8b61921c4f3411ef8f64816d994ee0
Anton Vasilyuk <47186291+Kennyfmx@users.noreply.github.com>
2021-02-15 16:10:53 +0300

    feat: Import more libraries to Bot

--- simpleBot_test.py
+++ simpleBot_test.py
@@ -1,5 +1,10 @@
-import random
 import nltk
+import random
+from sklearn.model_selection import train_test_split
+from sklearn.svm import LinearSVC
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+
+# TODO Update Config and Move to separate file
 
 BOT_CONFIG = {
     'intents': {
