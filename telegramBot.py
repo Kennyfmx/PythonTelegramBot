cc68d047b188527c82381f9690c8fed03ec33fd8
Anton Vasilyuk <47186291+Kennyfmx@users.noreply.github.com>
2021-02-15 16:31:01 +0300

    feat: Add functions for intent
    Add function for classify intent
    Add function for get answer by intent

--- telegramBot.py
+++ telegramBot.py
@@ -30,3 +30,33 @@ BOT_CONFIG = {
         'Скажи по-другому',
     ]
 }
+
+texts = []
+intent_names = []
+
+for intent, intent_data in BOT_CONFIG['intents'].items():
+    for example in intent_data['examples']:
+        texts.append(example)
+        intent_names.append(intent)  
+        
+vectorizer = TfidfVectorizer(ngram_range=(2, 4), analyzer='char')
+X = vectorizer.fit_transform(texts)
+clf = LinearSVC()
+clf.fit(X, intent_names)
+
+def classify_intent(replica):
+    intent = clf.predict(vectorizer.transform([replica]))[0]
+    # clf.classes_
+    examples = BOT_CONFIG['intents'][intent]['examples']
+    for example in examples:
+        example = clear_text(example)
+        if len(example) > 0:       
+            if abs(len(example) - len(replica)) / len(example) < 0.2:
+                distance = nltk.edit_distance(replica, example)
+                if len(example) and distance / len(example) < 0.3:
+                    return intent
+                
+def get_answer_by_intent(intent):
+    if intent in BOT_CONFIG['intents']:
+        responses = BOT_CONFIG['intents'][intent]['responses']
+        return random.choice(responses)
