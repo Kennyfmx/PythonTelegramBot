c07ecbb01cb7c49f1b7b218d30e22a4e68ec0650
Anton Vasilyuk <47186291+Kennyfmx@users.noreply.github.com>
2021-02-15 16:13:27 +0300

    init: start telegramBot_alpha
    Add libraries

---
+++ telegramBot.py
@@ -0,0 +1,5 @@
+import nltk
+import random
+from sklearn.model_selection import train_test_split
+from sklearn.svm import LinearSVC
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
